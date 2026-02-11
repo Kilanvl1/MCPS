6271c09c4b8305292bded65f7dd62c45d610c5d0
sedefcakmak04 <sedefcakmak04@gmail.com>
2026-02-11 22:30:08 +0100

    Update assignment1.cpp

--- src/assignment_1/assignment1.cpp
+++ src/assignment_1/assignment1.cpp
@@ -87,18 +87,18 @@ public:
     sc_inout_rv<64>           Port_MemData;
     sc_in<Memory::RetCode>    Port_MemDone;
 
-    SC_CTOR(Cache) : time_counter(0) {
+    SC_CTOR(Cache) : counter(0) {
         SC_THREAD(execute);
         sensitive << Port_CLK.pos();
         dont_initialize();
 
         // init all lines as invalid
-        for (int s = 0; s < NUM_SETS; s++) {
-            for (int w = 0; w < WAYS; w++) {
-                lines[s][w].valid = false;
-                lines[s][w].dirty = false;
-                lines[s][w].tag = 0;
-                lines[s][w].last_used = 0;
+        for (int i = 0; i < NUM_SETS; i++) {
+            for (int j = 0; j < WAYS; j++) {
+                lines[i][j].valid = false;
+                lines[i][j].dirty = false;
+                lines[i][j].tag = 0;
+                lines[i][j].last_used = 0;
             }
         }
     }
@@ -106,29 +106,28 @@ public:
 private:
 
     // Cache configuration
-    static const int LINE_SIZE   = 32;          // bytes
-    static const int CACHE_SIZE  = 32 * 1024;   // bytes
+    static const int LINE_SIZE   = 32; //bytes
+    static const int CACHE_SIZE  = 32 * 1024; //bytes
     static const int WAYS        = 8;
-    static const int NUM_SETS    = CACHE_SIZE / (LINE_SIZE * WAYS); // 128
+    static const int NUM_SETS    = CACHE_SIZE / (LINE_SIZE * WAYS); //128
 
-    static const int OFFSET_BITS = 5;  // log2(32)
-    static const int INDEX_BITS  = 7;  // log2(128)
+    static const int OFFSET_BITS = 5; //log2(32)
+    static const int INDEX_BITS  = 7; //log2(128)
 
-
-    // Cache line metadata
+    //Cache line metadata
     struct Line {
         bool valid;
         bool dirty;
         uint64_t tag;
-        uint64_t last_used; // timestamp for LRU
+        uint64_t last_used; //timestamp for LRU
     };
 
     Line lines[NUM_SETS][WAYS];
-    uint64_t time_counter;
+    uint64_t counter;
 
-    // Helpers
+    //Helpers
 
-    // get set index : remove the byte offset + mask the lower index bits (isolate set bits)
+    //get set index : remove the byte offset + mask the lower index bits (isolate set bits)
     uint32_t get_set(uint64_t addr) const {
         return (addr >> OFFSET_BITS) & ((1u << INDEX_BITS) - 1u);
     }
@@ -139,48 +138,48 @@ private:
     }
 
     //reconstruct base address to be used during eviction
-    uint64_t line_base_addr(uint64_t tag, uint32_t set) const {
+    uint64_t base_addr(uint64_t tag, uint32_t set) const {
         return (tag << (OFFSET_BITS + INDEX_BITS)) | (uint64_t(set) << OFFSET_BITS);
     }
 
     //search to determine if already cached
-    int find_hit_way(uint32_t set, uint64_t tag) const {
-        for (int w = 0; w < WAYS; w++) {
-            if (lines[set][w].valid && lines[set][w].tag == tag) { //hit if line valid + tag matches
-                return w;
+    int get_hit_index(uint32_t set, uint64_t tag) const {
+        for (int i = 0; i < WAYS; i++) {
+            if (lines[set][i].valid && lines[set][i].tag == tag) { //hit if line valid + tag matches
+                return i;
             }
         }
         return -1;
     }
 
-    // select cache line to replace (victim)
+    //select cache line to replace (victim)
     int choose_victim(uint32_t set) const {
         // Prefer invalid line
-        for (int w = 0; w < WAYS; w++) {
-            if (!lines[set][w].valid) return w;
+        for (int i = 0; i < WAYS; i++) {
+            if (!lines[set][i].valid) return i;
         }
-        // Else select with smallest LRU
+        //Else select with smallest LRU
         int victim = 0;
         uint64_t min_used = lines[set][0].last_used;
-        for (int w = 1; w < WAYS; w++) {
-            if (lines[set][w].last_used < min_used) {
-                min_used = lines[set][w].last_used;
-                victim = w;
+        for (int i = 1; i < WAYS; i++) {
+            if (lines[set][i].last_used < min_used) {
+                min_used = lines[set][i].last_used;
+                victim = i;
             }
         }
         return victim;
     }
 
-    // Issue a read request to memory and block until reply
-    void mem_read_blocking(uint64_t addr) {
+    //Issue a read request to memory and block until reply
+    void mem_read(uint64_t addr) {
         // Memory module models 100 cycles
         Port_MemAddr.write(addr);
         Port_MemFunc.write(Memory::FUNC_READ);
         wait(Port_MemDone.value_changed_event());
     }
 
-    // Issue a write request to main memory and block until memory replies
-    void mem_write_blocking(uint64_t addr, uint64_t data) {
+    //Issue a write request to main memory and block until memory replies
+    void mem_write(uint64_t addr, uint64_t data) {
 
         Port_MemAddr.write(addr);
         Port_MemFunc.write(Memory::FUNC_WRITE);
@@ -189,7 +188,6 @@ private:
         Port_MemData.write(data);
         wait();
         Port_MemData.write(float_64_bit_wire);
-
         wait(Port_MemDone.value_changed_event()); // memory completes after 100 cycles
     }
 
@@ -200,7 +198,7 @@ private:
             // return deterministic dummy value
             uint64_t data = addr * 10;
             Port_CPUData.write(data);
-            wait(); // allow CPU to sample
+            wait(); // for CPU to sample
             Port_CPUData.write(float_64_bit_wire); // release bus
 
             Port_CPUDone.write(Memory::RET_READ_DONE);
@@ -233,23 +231,23 @@ private:
             uint64_t tag = get_tag(addr);
 
             //check for hit
-            int hit_way = find_hit_way(set, tag);
+            int hit_index = get_hit_index(set, tag);
 
 
             // HIT
-            if (hit_way != -1) {
+            if (hit_index != -1) {
                 // Update stats + log
                 if (f == Memory::FUNC_READ) {
                     stats_readhit(0);
-                    log(name(), "read hit address =", addr, "set =", set, "line =", hit_way);
+                    log(name(), "read hit address =", addr, "set =", set, "line =", hit_index);
                 } else {
                     stats_writehit(0);
-                    lines[set][hit_way].dirty = true; // write-back
-                    log(name(), "write hit address =", addr, "set =", set, "line =", hit_way);
+                    lines[set][hit_index].dirty = true; // write-back
+                    log(name(), "write hit address =", addr, "set =", set, "line =", hit_index);
                 }
 
                 // Update LRU
-                lines[set][hit_way].last_used = ++time_counter;
+                lines[set][hit_index].last_used = ++counter;
 
                 // 1-cycle cache latency
                 wait(1);
@@ -275,7 +273,7 @@ private:
             // If victim valid and dirty : write-back --> eviction handling
             if (lines[set][victim].valid && lines[set][victim].dirty) {
                 uint64_t evict_tag  = lines[set][victim].tag;
-                uint64_t evict_addr = line_base_addr(evict_tag, set);
+                uint64_t evict_addr = base_addr(evict_tag, set);
 
                 log(name(), "evict dirty line",
                     "evict_addr =", evict_addr,
@@ -283,13 +281,13 @@ private:
                     "tag =", evict_tag);
 
                 // Write-back takes 100 cycles via memory
-                mem_write_blocking(evict_addr, evict_addr * 10);
+                mem_write(evict_addr, evict_addr * 10);
 
                 lines[set][victim].dirty = false;
             } else if (lines[set][victim].valid) {
                 // Clean eviction : no write back - log
                 uint64_t evict_tag  = lines[set][victim].tag;
-                uint64_t evict_addr = line_base_addr(evict_tag, set);
+                uint64_t evict_addr = base_addr(evict_tag, set);
 
                 log(name(), "evict clean line",
                     "evict_addr =", evict_addr,
@@ -298,13 +296,13 @@ private:
             }
 
             // Allocate-on-write / read
-            mem_read_blocking(addr);
+            mem_read(addr);
 
             // Install new line
             lines[set][victim].valid = true;
             lines[set][victim].tag = tag;
             lines[set][victim].dirty = (f == Memory::FUNC_WRITE);
-            lines[set][victim].last_used = ++time_counter;
+            lines[set][victim].last_used = ++counter;
 
             // Log completion (addr + set + line)
             if (f == Memory::FUNC_READ) {
@@ -422,12 +420,10 @@ SC_MODULE(CPU) {
     }
 };
 
-
 int sc_main(int argc, char *argv[]) {
     sc_report_handler::set_verbosity_level(SC_MEDIUM);
     // Uncomment the next line to silence the log() messages.
     // sc_report_handler::set_verbosity_level(SC_LOW);
-
 
     try {
         // Get the tracefile argument and create Tracefile object
